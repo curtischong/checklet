61c3654f73b2771e1d9989673a76cb5888e48273
Curtis Chong <curtischong5@gmail.com>
2022-06-15 17:54:22 -0400

    spacy (#15)

--- core/converters/tokenizer.py
+++ core/converters/tokenizer.py
@@ -1,7 +1,13 @@
 from typing import List
 
+import yaml
+
 
 class Tokenizer:
-    def tokenize(input: str) -> List[str]:
-        # TODO: use spacey
-        return [input]
+    def __init__(self, config: str):
+        with open(config["model_location"], 'r') as stream:
+            data = yaml.safe_load(stream)
+            self.tokenizer = data["spacy_model"]
+
+    def tokenize(self, doc: str) -> List[str]:
+        return self.tokenizer(doc)
