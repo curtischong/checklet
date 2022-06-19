2b77c3f063d1f5d1e91cd1c6d4450255e9e39f8c
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2022-06-18 22:40:06 -0400

    add spacy requirment (#30)

--- backend/release-tasks.sh
+++ backend/release-tasks.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-spacy download en_core_web_sm
 python src/manage.py migrate
--- core/setup.py
+++ core/setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 install_requires = [
     "spacy==3.3.1",
     "PyYAML==6.0",
+    "en-core-web-sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.3.0/en_core_web_sm-3.3.0-py3-none-any.whl"
 ]
 
 setup(
