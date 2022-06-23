462d3079a9c57115c8ee628f0bf1e96c0438c3b4
Curtis Chong <curtischong5@gmail.com>
2022-06-23 00:00:14 -0400

    Added Constituency Tree: NautTree (#42)
    * Added NautTree
    
    * NautTrees now have pointers to NautTokens

--- core/core/converters/input/naut_parser.py
+++ core/core/converters/input/naut_parser.py
@@ -1,5 +1,10 @@
+from __future__ import annotations
+
+from typing import List, Generator
+
 import stanza
 from stanza.models.common.doc import Document, Sentence, Token
+from stanza.models.constituency.parse_tree import Tree
 
 
 # tokens are contiguous pieces of characters that are separated by whitespace/punctuation
@@ -31,14 +36,96 @@ class NautToken:
         return str(self.text)
 
 
-# TODO: consider storing the tokens as a constituent tree
+class NautTree:
+    def __init__(self, label: str):
+        self.label = label
+        self.parent = None
+        self.children = []
+        self.token = None  # When this NautTree is a leaf node, self.token points to the NautToken that follows this constituency label
+
+    def is_leaf(self):
+        return len(self.children) == 0
+
+    def set_token(self, token: NautToken):
+        if not self.is_leaf():
+            print("WARNING: setting a token to a non-leaf node")
+        self.token = token
+
+    def add_child(self, child_tree: NautTree) -> None:
+        child_tree.parent = self
+        self.children.append(child_tree)
+
+    # the NautTokens that are the leaf nodes of this subtree
+    def leaves(self) -> List[NautTree]:
+        if self.is_leaf():
+            return [self]
+        leaves = []
+        for child in self.children:
+            leaves.extend(child.leaves())
+        return leaves
+
+    # returns all tokens under this subtree
+    def tokens(self) -> List[NautToken]:
+        leaves = self.leaves()
+        return [leaf.token for leaf in leaves]
+
+    # a generator for preorder traversal on all nodes of the tree (NautTokens excluded)
+    def preorder(self) -> Generator[NautTree, None, None]:
+        yield self
+        for child in self.children:
+            if isinstance(child, NautTree):
+                yield from child.preorder()
+
+    def __repr__(self) -> str:
+        if self.is_leaf():
+            return "({} {})".format(self.label, str(self.token))
+
+        children_repr = " ".join([str(child) for child in self.children])
+        return "({} {})".format(self.label, children_repr)
+
+
 class NautSent:
     def __init__(self, sentence: Sentence):
         sentence.naut_sent = self
         self.sentence = sentence
         self.tokens = [NautToken(token) for token in sentence.tokens]
         self.entities = sentence.entities
+        self.constituency_tree = self._parse_naut_tree(sentence, self.tokens)
         # TODO: have an array of named entities. I think we extract them from the tokens
+
+    # this deep-copies the stanza constituency tree and converts it to naut trees
+    def _parse_naut_tree(self, sentence: Sentence, tokens: List[NautToken]) -> NautTree:
+        root: Tree = sentence.constituency
+        token_idx = 0
+        # the idea is to perform DFS to deep copy the Stanza nodes into NautTree Nodes
+        root_parent = NautTree("dummy parent")
+        stack = [(root, root_parent)]
+
+        while stack:
+            stanza_nd, naut_parent = stack.pop()
+            if stanza_nd.is_leaf():
+                # We want the leaf nodes of the NautTree to reference a NautToken
+                text = stanza_nd.label
+                if text != tokens[token_idx].text:  # validate that the leaf node is the ith NautToken
+                    print("WARNING: the NautToken text mismatches from the constituency leaf text")
+                naut_token = tokens[token_idx]
+                token_idx += 1
+                naut_parent.set_token(naut_token)
+                continue
+            naut_nd = NautTree(stanza_nd.label)
+            naut_parent.add_child(naut_nd)
+
+            # Append this node's children to the stack
+            # Note: we append in reverse order since we want to visit
+            # the child nodes from left to right (so we traverse the leaf
+            # nodes in chronological order)
+            for nxt in range(len(stanza_nd.children) - 1, -1, -1):
+                child = stanza_nd.children[nxt]
+                stack.append((child, naut_nd))
+
+        naut_root = root_parent.children[0]
+        naut_root.parent = None  # remove the pointer to the dummy parent
+        return naut_root
 
     def __repr__(self):
         return str(self.tokens)
--- core/core/converters/input/naut_parser_test.py
+++ core/core/converters/input/naut_parser_test.py
@@ -1,9 +1,17 @@
 from naut_parser import NautParser
 
+
 def run_test():
     np = NautParser()
-    doc = np.parse("this is a really long sentence.")
-    print(doc.words)
+    doc = np.parse("this is a really long sentence")
+    # doc = np.parse(
+    #    "Use yield from with your recursive call, otherwise you are just ignoring the result of the recursive call.")
+    constituency_tree = doc.sentences[0].constituency_tree
+    print(constituency_tree.leaves())
+    print(constituency_tree.tokens())
+    for node in constituency_tree.preorder():
+        print(node.label)
+
 
 if __name__ == "__main__":
     run_test()
