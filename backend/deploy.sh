bb99974343fed6de2fec823ac93edf96b47b280b
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2022-07-10 21:07:55 -0400

    deploy backend action (#97)
    * start script
    
    * fix script
    
    * add git hash for loggin
    
    * add git hash for loggin

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
+echo "Running deployment for $1"
 gcloud compute scp --recurse ../backend instance-1:~/ --recurse --zone "northamerica-northeast2-a" --project "backend-nautilus"
-gcloud compute scp --recurse ../core instance-1:~/ --recurse --zone "northamerica-northeast2-a" --project "backend-nautilus"
-
-# pip install -r requirements.txt
-# make run > prodlogs 2>&1 &
+# gcloud compute scp --recurse ../core instance-1:~/ --recurse --zone "northamerica-northeast2-a" --project "backend-nautilus"
+gcloud compute ssh --zone "northamerica-northeast2-a" "instance-1"  --project "backend-nautilus" --command "cd backend/ && ./prod_run.sh $1"
---
+++ backend/prod_run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Deploying prod for $1"
+
+CURR_PID=$(pgrep -f waitress)
+kill -9 $CURR_PID
+
+pip install -r requirements.txt
+make run-prod > prodlogs-$1 2>&1 &
