f56a138cc534b79421f5ea7f5c25776db033a901
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2022-07-25 19:14:21 -0400

    fix test (#149)

--- core/core/task/library/extract_verb_conjunctions_at_sentence_start_test.py
+++ core/core/task/library/extract_verb_conjunctions_at_sentence_start_test.py
@@ -1,19 +1,21 @@
+from core.converters.input.naut_parser import NautParser
 from core.task.library.extract_verb_conjunctions_at_sentence_start_task import \
     extract_verb_conjunctions_at_sentence_start_task
-from core.task.test.task_parsing_helper import TaskParsingHelper
 
 
 class TestExtractVerbConjunctionsAtSentenceStart:
     def test_verb_conjunction_exists(self):
+        naut_parser = NautParser()
         input = "led and organized a retreat"
-        doc = TaskParsingHelper().naut_parser.parse(input)
+        doc = naut_parser.parse(input)
         res = extract_verb_conjunctions_at_sentence_start_task(doc)
         assert len(res) == 1
         result_text = [str(token) for token in res[0]]
         assert result_text == ["led", "organized"]
 
     def test_verb_conjunction_absent(self):
+        naut_parser = NautParser()
         input = "organized a retreat"
-        doc = TaskParsingHelper().naut_parser.parse(input)
+        doc = naut_parser.parse(input)
         res = extract_verb_conjunctions_at_sentence_start_task(doc)
         assert not res
