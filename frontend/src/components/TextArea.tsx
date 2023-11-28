4ae19dc9ccfba548c18b63907347da0a7bce78b5
Curtis Chong <curtischong5@gmail.com>
2023-11-28 13:49:58 -0500

    textareas now automatically grow

--- frontend/src/components/TextArea.tsx
+++ frontend/src/components/TextArea.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 export type ITextArea = React.DetailedHTMLProps<
     React.TextareaHTMLAttributes<HTMLTextAreaElement>,
     HTMLTextAreaElement
@@ -8,10 +10,21 @@ export const TextArea: React.FC<ITextArea> = ({
     children,
     ...rest
 }) => {
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const MIN_HEIGHT = 100;
+    useEffect(() => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = "auto";
+            textareaRef.current.style.height =
+                Math.max(textareaRef.current.scrollHeight + 20, MIN_HEIGHT) +
+                "px";
+        }
+    }, [rest.value]);
     return (
         <textarea
-            className={`bg-gray-100 p-1 my-1 text-gray-600 border border-[#5384d4] py-2 px-4 rounded ${className}`}
+            className={`bg-gray-100 p-1 my-1 text-gray-600 border border-[#5384d4] py-2 px-4 rounded resize-none ${className}`}
             {...rest}
+            ref={textareaRef}
         >
             {children}
         </textarea>
