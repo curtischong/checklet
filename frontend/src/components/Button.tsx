a402c9569b67bf325089a9df9211a720b2cf5fd5
Curtis Chong <curtischong5@gmail.com>
2023-11-27 21:00:01 -0500

    fixed tailwind jit compilation

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -27,7 +27,7 @@ export const NormalButton: React.FC<IButton> = ({
 }) => {
     return (
         <button
-            className={`bg-gray-100 hover:bg-gray-200 text-gray-600 hover:text-gray-800 py-2 px-4 rounded transition duration-300 ${className}`}
+            className={`bg-gray-100 hover:bg-[#5384d4] text-gray-600 border border-[#5384d4] hover:text-white py-2 px-4 rounded transition duration-300 ${className}`}
             {...rest}
         >
             {children}
