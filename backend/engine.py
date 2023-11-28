56c88c51b1fdd3290f9500906d7549b51d27d26a
Curtis Chong <curtischong5@gmail.com>
2023-11-28 00:15:06 -0500

    started to work on checker parsing in the engine

--- backend/engine.py
+++ backend/engine.py
@@ -1,12 +1,23 @@
 from backend.check import Check
+import os, json
+
+from backend.checker import Checker
 
 
 class Engine:
     def __init__(self):
-        pass
+        self.checkers = []
 
     def generate_feedback(self):
         pass
+
+    def import_checkers(self):
+        checkers_dir = './checkers/'
+        for filename in os.listdir(checkers_dir):
+            if filename.endswith('.json'):
+                checker_path = os.path.join(checkers_dir, filename)
+                checker = Checker(checker_path)
+
 
     def checkDoc(self, document:str, check: Check):
         # todo: feed prompt to llm
