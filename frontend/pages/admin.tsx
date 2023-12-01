3203342b6911975cb2212695b0ad5bdb8ae44877
Curtis Chong <curtischong5@gmail.com>
2023-12-01 11:14:38 -0500

    added admin page to create rizzume

---
+++ frontend/pages/admin.tsx
@@ -0,0 +1,52 @@
+import { Api } from "@api/apis";
+import { NormalButton } from "@components/Button";
+import { CheckerBlueprint } from "@components/create-checker/CheckerTypes";
+import { useClientContext } from "@utils/ClientContext";
+import { createUniqueId } from "@utils/strings";
+import { useCallback } from "react";
+
+const AdminPage: React.FC = () => {
+    const { user } = useClientContext();
+    if (user === null || user.email !== "curtischong5@gmail.com") {
+        return <div>Not logged in as an admin</div>;
+    }
+
+    const createRizzume = useCallback(async () => {
+        const checkerBlueprint: CheckerBlueprint = {
+            name: "Rizzume",
+            desc: "Rizz up your resume",
+            checkBlueprints: [
+                {
+                    name: "Shorten Month",
+                    instruction: `If you see the name of the month, shorten it to only three characters. Do not end these shortened months with a period.`,
+                    longDesc: `Shorter months create more whitespace.`,
+                    category: "Whitespace",
+                    positiveExamples: [
+                        {
+                            originalText: "January",
+                            editedText: "Jan",
+                        },
+                    ],
+                    checkId: createUniqueId(),
+                },
+            ],
+            id: createUniqueId(),
+            creatorId: user.uid,
+        };
+        Api.createChecker(
+            checkerBlueprint,
+            checkerBlueprint.id,
+            await user.getIdToken(),
+        );
+    }, []);
+    return (
+        <div className="container mx-auto mt-10 flex flex-col px-32">
+            <p className="text-2xl text-center">Admin</p>
+            <NormalButton className="mt-10 w-60" onClick={createRizzume}>
+                Create Rizzume
+            </NormalButton>
+        </div>
+    );
+};
+
+export default AdminPage;
