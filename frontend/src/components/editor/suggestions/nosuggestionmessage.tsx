aba1b8ce707463a4263879de3f25b3e7ea58bf64
Curtis Chong <curtischong5@gmail.com>
2023-12-05 13:06:35 -0500

    removed css page for nosuggestionmessage component

--- frontend/src/components/editor/suggestions/nosuggestionmessage.tsx
+++ frontend/src/components/editor/suggestions/nosuggestionmessage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import css from "./nosuggestionmessage.module.scss";
 import { SuggestionMessage } from "./suggestionsTypes";
 
 export const NoSuggestionMessage: React.FC<SuggestionMessage> = (
@@ -8,10 +7,12 @@ export const NoSuggestionMessage: React.FC<SuggestionMessage> = (
     const { imageSrc, header, content } = suggestionMessage;
 
     return (
-        <div className={css.container}>
-            <img src={imageSrc} className={css.image} />
-            <div className={css.header}>{header}</div>
-            <div className={css.content}>{content}</div>
+        <div className="flex flex-col items-center text-center m-auto pt-8">
+            <img src={imageSrc} className="h-[18.75rem]" />
+            <div className="font-bold py-2">{header}</div>
+            <div className="flex flex-col items-center justify-center">
+                {content}
+            </div>
         </div>
     );
 };
