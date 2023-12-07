2cc0fbc470b78e421fe655f4f067161059e87eef
Curtis Chong <curtischong5@gmail.com>
2023-12-07 14:33:54 -0500

    improved how the create checker page looks

--- frontend/src/components/create-checker/CheckOverview.tsx
+++ frontend/src/components/create-checker/CheckOverview.tsx
@@ -25,7 +25,7 @@ export const CheckOverview = ({
     return (
         <div className="border bg-white rounded-md shadow-around py-3 px-6 w-[400px]">
             <div className="flex flex-row items-end">
-                <div className="text-lg font-bold">
+                <div className="text-xl border-b-4 border-solid border-blue-300">
                     {checkBlueprint.objInfo.name}
                 </div>
                 <div className="ml-4 mb-[2px]">{checkBlueprint.category}</div>
@@ -38,8 +38,8 @@ export const CheckOverview = ({
                 </div>
             </div>
             {/* <h2>{checkBlueprint.instruction}</h2> */}
-            <h2>{checkBlueprint.objInfo.desc}</h2>
-            <h2 className="font-bold mt-4">Positive Examples</h2>
+            <h2 className="mt-4">{checkBlueprint.objInfo.desc}</h2>
+            <h2 className="font-bold mt-5">Positive Examples</h2>
             <div className="flex flex-col">
                 {/* TODO: do a diff, so we see the red deletion / green insertion */}
                 {checkBlueprint.positiveExamples.map((example, idx) => (
@@ -52,6 +52,7 @@ export const CheckOverview = ({
                 ))}
             </div>
             <LabelWithSwitch
+                className="mt-6"
                 text="Is Enabled:"
                 helpText="The checker only runs enables checks on the document"
                 isChecked={tmpIsChecked}
