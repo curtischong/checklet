61b7ca90d81c2d2e46182c6b0607b004724063a9
Curtis Chong <curtischong5@gmail.com>
2023-12-07 20:02:38 -0500

    wip generic navigation bar

--- frontend/src/components/create-check/CreateCheckNavigationPath.tsx
+++ frontend/src/components/create-check/CreateCheckNavigationPath.tsx
@@ -1,31 +1,53 @@
 import { RightArrowIcon } from "@components/icons/RightArrowIcon";
+import classNames from "classnames";
 import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
+import React from "react";
 
-export const CreateCheckNavigationPath = (): JSX.Element => {
+export type LinkSection = {
+    name: string;
+    url: string;
+    query?: () => ParsedUrlQuery;
+};
+
+interface Props {
+    sections: LinkSection[];
+}
+
+export const CreateCheckNavigationPath = ({ sections }: Props): JSX.Element => {
     const router = useRouter();
-    const checkerId = router.query.checkerId;
+
     return (
         <div className="flex flex-row items-center">
-            <p
-                className="text-gray-400 cursor-pointer  transition duration-300 hover:text-gray-600"
-                onClick={() => {
-                    router.push("/dashboard");
-                }}
-            >
-                Dashboard
-            </p>
-            <RightArrowIcon className="mx-2 w-[14px]" />
-            <p
-                className="text-gray-400 cursor-pointer  transition duration-300 hover:text-gray-600"
-                onClick={() => {
-                    // TODO: we need to have a function that will rout to the right path, but iwll also consider al lthe right url params
-                    router.push(`/create/checker/${checkerId}`);
-                }}
-            >
-                Create checker
-            </p>
-            <RightArrowIcon className="mx-2 w-[14px]" />
-            <p className="font-bold text-gray-600">Create check</p>
+            {sections.map((section, index) => {
+                const isLastSection = index === sections.length - 1;
+                return (
+                    <React.Fragment key={index}>
+                        <p
+                            className={classNames(
+                                "cursor-pointer transition duration-300 ",
+                                {
+                                    "text-gray-400 hover:text-gray-600":
+                                        !isLastSection,
+                                    "text-gray-600 hover:text-gray-600":
+                                        isLastSection,
+                                },
+                            )}
+                            onClick={() => {
+                                router.push(
+                                    section.url,
+                                    section.query ? section.query() : undefined,
+                                );
+                            }}
+                        >
+                            {section.name}
+                        </p>
+                        {index < sections.length - 1 && (
+                            <RightArrowIcon className="mx-2 w-[14px]" />
+                        )}
+                    </React.Fragment>
+                );
+            })}
         </div>
     );
 };
