a3659791ff24dc4a70e7eb661910d756d130e217
Curtis Chong <curtischong5@gmail.com>
2023-12-09 18:05:41 -0500

    blank homepage (#24)
    * wip homepage
    
    * blank home page

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,43 @@
+import { CheckerStore } from "@components/CheckerStore";
+
+const HomePage: React.FC = () => {
+    // const router = useRouter();
+    return (
+        <div className="flex flex-row">
+            <div className="container mx-auto text-center mt-20">
+                <div className="ml-0">
+                    <p className="text-4xl">I need a name lol</p>
+                    <p>
+                        This is Grammarly, but for anything. Writing jokes,
+                        resumes, cold emails, etc.
+                    </p>
+                </div>
+                <CheckerStore />
+
+                {/* <NormalButton
+                    onClick={() => {
+                        router.push("/editor");
+                    }}
+                >
+                    Try fixing my work!
+                </NormalButton>
+                <div>
+                    We ask experts for help all the time. Because they help out
+                    with our specific need
+                </div>
+                <div>Online courses don't work. Why? </div>
+                <div>How it works</div>
+                <div>1.</div>
+                <div>Paste your resume/cold email/movie script</div>
+                <div>2.</div>
+                <div>
+                    Get tailored suggestions written by experts in the field
+                </div>
+                <div>3.</div>
+                <div>Edit your document!</div> */}
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
