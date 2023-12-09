1a7cd6733326a27d9c26fe7ba6b324e159104295
Curtis Chong <curtischong5@gmail.com>
2023-12-09 16:51:53 -0500

    cleanup

--- frontend/shared/checker-utils.ts
+++ frontend/shared/checker-utils.ts
@@ -1,6 +1,8 @@
 import { Checker } from "@api/checker";
 import { CheckDescObj } from "@components/create-checker/CheckerTypes";
 
+// this file needs to be in this directory since it's used by the client and the server.
+// if we place it with the other functions, the client will import server dependencies (that it cannot get)
 export const getCheckDescForCheckIds = (
     checker: Checker,
     uniqueCheckIds: Set<string>,
@@ -8,7 +10,6 @@ export const getCheckDescForCheckIds = (
     const checkDescObj: CheckDescObj = {};
 
     for (const checkId of uniqueCheckIds) {
-        console.log("checkId", checkId);
         const checkBlueprint = checker.checks.get(checkId)?.blueprint;
         if (!checkBlueprint) {
             console.error(
