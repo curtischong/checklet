d85bea80d3355268e7a0783fe8f5286e4f5d1d43
Curtis Chong <curtischong5@gmail.com>
2023-12-12 11:08:09 -0500

    Fix build (#91)
    * fixing building for the app
    
    * fix deploy

--- checker-paige/src/pages/privacy-policy.tsx
+++ checker-paige/src/pages/privacy-policy.tsx
@@ -2,7 +2,7 @@ const PrivacyPolicy: React.FC = () => {
     return (
         <div className="flex">
             <div className="container mx-auto text-center mt-20">
-                <p>TODO... but I won't sell your data</p>
+                <p>TODO... but I won&apos;t sell your data</p>
             </div>
         </div>
     );
