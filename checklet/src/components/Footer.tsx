1ce637ad4a7b6bbf69a78556ef59644d170c2fcd
Curtis Chong <curtischong5@gmail.com>
2023-12-13 22:40:59 -0500

    functional footer (#98)

---
+++ checklet/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+    return (
+        <div className="bg-[#f7b7b8] w-full py-4 mt-10 flex flex-row space-x-4  justify-center">
+            {/* <div className="flex flex-row space-x-4 justify-center bottom-0 relative"> */}
+            <a href="/privacy-policy">Privacy Policy</a>
+            <a href="/terms-of-service">Terms of Service</a>
+            {/* </div> */}
+        </div>
+    );
+};
--- checklet/src/pages/index.tsx
+++ checklet/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { NormalButton } from "@/components/Button";
 import { StoreFront } from "@/components/CheckerStore";
+import { Footer } from "@/components/Footer";
 import ThinLine from "@/components/ThinLine";
 import { CursorIcon } from "@/components/icons/CursorIcon";
 import { DownArrowWithTailIcon } from "@/components/icons/DownArrowWithTailIcon";
@@ -16,123 +17,124 @@ import { useRouter } from "next/router";
 const HomePage: React.FC = () => {
     const router = useRouter();
     return (
-        <div className="container mx-auto text-center px-3">
-            <div className="ml-0 h-[100vh] flex flex-col justify-center">
+        <>
+            <div className="container mx-auto text-center px-3">
+                <div className="ml-0 h-[100vh] flex flex-col justify-center">
+                    <Image
+                        alt="SpacyChecklet"
+                        src={SpacyChecklet.src}
+                        width={200}
+                        height={200}
+                        className="h-[8rem] top-32 left-[20%] absolute"
+                    />
+                    <Image
+                        alt="LoveChecklet"
+                        src={LoveChecklet.src}
+                        width={200}
+                        height={200}
+                        className="h-[6rem] bottom-[15rem] right-[5%] md:right-[20%] absolute"
+                    />
+                    <Image
+                        alt="DockyChecklet"
+                        src={DockyChecklet.src}
+                        width={200}
+                        height={200}
+                        className="h-[6rem] bottom-[10rem] left-[5%] md:left-[30%] absolute"
+                    />
+                    <p className="text-5xl font-mackinac">Checklet.page</p>
+                    <p className="mt-4">
+                        Expert-written checkers to polish jokes, edit resumes,
+                        revise emails... check anything!
+                    </p>
+                    <div
+                        className="border-[2px] border-gray-500 hover:border-gray-600 rounded-[100px] w-[40px] h-[40px] absolute bottom-8 left-[calc(50%-20px)] cursor-pointer"
+                        onClick={() =>
+                            window.scrollBy({
+                                left: 0,
+                                top: window.innerHeight,
+                                behavior: "smooth",
+                            })
+                        }
+                    >
+                        <DownArrowWithTailIcon className="mx-auto mt-[5px]" />
+                    </div>
+                </div>
+                <div className="text-3xl mt-32 font-mackinac">How it works</div>
+                <ThinLine />
+                <div className="flex flex-row justify-center space-x-8 mt-10">
+                    <div className="text-lg">
+                        1. Select a Checker for your type of writing
+                    </div>
+                    <div className="relative">
+                        <StoreFront
+                            storefront={{
+                                objInfo: {
+                                    name: "Rizzume",
+                                    desc: "Rizz up your resume to dazzle any employer. It will make points sharp and salient. All to make you sound impressive.",
+                                    id: createShortId(),
+                                    creatorId: "fakeuser",
+                                },
+                                placeholder: "paste yourresume",
+                            }}
+                            isDemo={true}
+                        />
+                        <CursorIcon className="w-[40px] h-[40px] right-4  bottom-[-20px] absolute" />
+                        <Image
+                            alt="PennyChecklet"
+                            src={PennyChecklet.src}
+                            width={200}
+                            height={200}
+                            className="h-[7rem] top-[10rem] right-[20%] absolute"
+                        />
+                    </div>
+                </div>
+
+                <div className="flex flex-row justify-center space-x-8 mt-40">
+                    <div className="text-2xl">
+                        2. Paste your writing into the editor
+                    </div>
+                    <div className="relative">
+                        <StoreFront
+                            storefront={{
+                                objInfo: {
+                                    name: "Rizzume",
+                                    desc: "Rizz up your resume to dazzle any employer. It will make points sharp and salient. All to make you sound impressive.",
+                                    id: createShortId(),
+                                    creatorId: "fakeuser",
+                                },
+                                placeholder: "paste yourresume",
+                            }}
+                        />
+                        <CursorIcon className="w-[40px] h-[40px] right-4  bottom-[-20px] absolute" />
+                        <Image
+                            alt="DerpChecklet"
+                            src={DerpChecklet.src}
+                            width={200}
+                            height={200}
+                            className="h-[5rem] top-[10rem] left-[30%] absolute"
+                        />
+                    </div>
+                </div>
                 <Image
-                    alt="SpacyChecklet"
-                    src={SpacyChecklet.src}
+                    alt="MushyChecklet"
+                    src={MushyChecklet.src}
                     width={200}
                     height={200}
-                    className="h-[8rem] top-32 left-[20%] absolute"
+                    className="h-[5rem] top-[70rem] right-[30%] absolute"
                 />
-                <Image
-                    alt="LoveChecklet"
-                    src={LoveChecklet.src}
-                    width={200}
-                    height={200}
-                    className="h-[6rem] bottom-[15rem] right-[5%] md:right-[20%] absolute"
-                />
-                <Image
-                    alt="DockyChecklet"
-                    src={DockyChecklet.src}
-                    width={200}
-                    height={200}
-                    className="h-[6rem] bottom-[10rem] left-[5%] md:left-[30%] absolute"
-                />
-                <p className="text-5xl font-mackinac">Checklet.page</p>
-                <p className="mt-4">
-                    Expert-written checkers to polish jokes, edit resumes,
-                    revise emails... check anything!
-                </p>
-                <div
-                    className="border-[2px] border-gray-500 hover:border-gray-600 rounded-[100px] w-[40px] h-[40px] absolute bottom-8 left-[calc(50%-20px)] cursor-pointer"
-                    onClick={() =>
-                        window.scrollBy({
-                            left: 0,
-                            top: window.innerHeight,
-                            behavior: "smooth",
-                        })
-                    }
+                <div className="flex flex-row">
+                    <div className="text-2xl">3. Receive instant feedback</div>
+                </div>
+                <NormalButton
+                    onClick={() => {
+                        router.push("/editor");
+                    }}
                 >
-                    <DownArrowWithTailIcon className="mx-auto mt-[5px]" />
-                </div>
-            </div>
-            <div className="text-3xl mt-32 font-mackinac">How it works</div>
-            <ThinLine />
-            <div className="flex flex-row justify-center space-x-8 mt-10">
-                <div className="text-lg">
-                    1. Select a Checker for your type of writing
-                </div>
-                <div className="relative">
-                    <StoreFront
-                        storefront={{
-                            objInfo: {
-                                name: "Rizzume",
-                                desc: "Rizz up your resume to dazzle any employer. It will make points sharp and salient. All to make you sound impressive.",
-                                id: createShortId(),
-                                creatorId: "fakeuser",
-                            },
-                            placeholder: "paste yourresume",
-                        }}
-                        isDemo={true}
-                    />
-                    <CursorIcon className="w-[40px] h-[40px] right-4  bottom-[-20px] absolute" />
-                    <Image
-                        alt="PennyChecklet"
-                        src={PennyChecklet.src}
-                        width={200}
-                        height={200}
-                        className="h-[7rem] top-[10rem] right-[20%] absolute"
-                    />
-                </div>
-            </div>
+                    Try it out
+                </NormalButton>
+                {/* <CheckerStore /> */}
 
-            <div className="flex flex-row justify-center space-x-8 mt-40">
-                <div className="text-2xl">
-                    2. Paste your writing into the editor
-                </div>
-                <div className="relative">
-                    <StoreFront
-                        storefront={{
-                            objInfo: {
-                                name: "Rizzume",
-                                desc: "Rizz up your resume to dazzle any employer. It will make points sharp and salient. All to make you sound impressive.",
-                                id: createShortId(),
-                                creatorId: "fakeuser",
-                            },
-                            placeholder: "paste yourresume",
-                        }}
-                    />
-                    <CursorIcon className="w-[40px] h-[40px] right-4  bottom-[-20px] absolute" />
-                    <Image
-                        alt="DerpChecklet"
-                        src={DerpChecklet.src}
-                        width={200}
-                        height={200}
-                        className="h-[5rem] top-[20rem] left-[30%] absolute"
-                    />
-                </div>
-            </div>
-            <Image
-                alt="MushyChecklet"
-                src={MushyChecklet.src}
-                width={200}
-                height={200}
-                className="h-[5rem] top-[70rem] right-[30%] absolute"
-            />
-            <div className="flex flex-row">
-                <div className="text-2xl">3. Receive instant feedback</div>
-            </div>
-            <NormalButton
-                onClick={() => {
-                    router.push("/editor");
-                }}
-            >
-                Try it out
-            </NormalButton>
-            {/* <CheckerStore /> */}
-
-            {/* <NormalButton
+                {/* <NormalButton
                     onClick={() => {
                         router.push("/editor");
                     }}
@@ -151,7 +153,9 @@ const HomePage: React.FC = () => {
                 <div>Edit your document!</div> 
                 <p>Special thanks to Joseph Anderson and James Portnow for being fantastic writers and inspiring me to make this/write in general</p>
                 */}
-        </div>
+            </div>
+            <Footer />
+        </>
     );
 };
 
