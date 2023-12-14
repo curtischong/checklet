16f8617fdab5bbf8d4036be8ae21b464340a27c0
Curtis Chong <curtischong5@gmail.com>
2023-12-13 22:25:33 -0500

    added checklets on blank pages

--- checklet/src/pages/editor/[checkerId].tsx
+++ checklet/src/pages/editor/[checkerId].tsx
@@ -2,7 +2,11 @@ import { Api } from "@api/apis";
 import { CheckerStore } from "@components/CheckerStore";
 import { CheckerStorefront } from "@components/create-checker/CheckerTypes";
 import { Editor } from "@components/editor/Editor";
+import LoveChecklet from "@public/checklets/love.svg";
+import PennyChecklet from "@public/checklets/penny.svg";
+import SpacyChecklet from "@public/checklets/spacy.svg";
 import { useClientContext } from "@utils/ClientContext";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
@@ -33,14 +37,35 @@ const EditorPage: React.FC = () => {
     } else if (storefront === "not found") {
         return (
             <div className="flex flex-col">
-                <div className="mx-auto mt-20 text-2xl font-mackinac">
+                <div className="mx-auto mt-20 text-3xl font-bold font-mackinac">
                     Checker not found.
                 </div>
-                <div className="mx-auto mt-4text-lg font-mackinac">
+                <div className="mx-auto mt-2">
                     It may have been made private. You can find other checkers
                     below
                 </div>
                 <CheckerStore />
+                <Image
+                    alt="LoveChecklet"
+                    src={LoveChecklet.src}
+                    width={200}
+                    height={200}
+                    className="h-[6rem] bottom-[15rem] right-[0%] md:right-[1%] absolute"
+                />
+                <Image
+                    alt="PennyChecklet"
+                    src={PennyChecklet.src}
+                    width={200}
+                    height={200}
+                    className="h-[7rem] top-[10rem] right-[7%] sm:right-[10%] md:right-[15%] absolute"
+                />
+                <Image
+                    alt="SpacyChecklet"
+                    src={SpacyChecklet.src}
+                    width={200}
+                    height={200}
+                    className="h-[8rem] top-32 left-[5%] absolute"
+                />
             </div>
         );
     } else {
