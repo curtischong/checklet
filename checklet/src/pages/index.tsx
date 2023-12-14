3fa992931d0c0e7b560978817ba2be017801e89d
Curtis Chong <curtischong5@gmail.com>
2023-12-14 00:10:33 -0500

    instructions are now better formatted

--- checklet/src/pages/index.tsx
+++ checklet/src/pages/index.tsx
@@ -2,6 +2,8 @@ import { NormalButton } from "@/components/Button";
 import { StoreFront } from "@/components/CheckerStore";
 import { Footer } from "@/components/Footer";
 import ThinLine from "@/components/ThinLine";
+import { CheckPreview } from "@/components/create-check/CheckPreview";
+import { CheckType } from "@/components/create-checker/CheckerTypes";
 import { CursorIcon } from "@/components/icons/CursorIcon";
 import { DownArrowWithTailIcon } from "@/components/icons/DownArrowWithTailIcon";
 import { createShortId } from "@/utils/strings";
@@ -18,7 +20,7 @@ const HomePage: React.FC = () => {
     const router = useRouter();
     return (
         <div>
-            <div className="container mx-auto text-center px-3">
+            <div className="container mx-auto text-center px-6">
                 <div className="ml-0 h-[100vh] flex flex-col justify-center">
                     <Image
                         alt="SpacyChecklet"
@@ -51,8 +53,16 @@ const HomePage: React.FC = () => {
                     <p className="text-5xl font-mackinac">Checklet.page</p>
                     <p className="mt-4">
                         Expert-written checkers to polish jokes, edit resumes,
-                        revise emails... check anything!
+                        revise emails... and check anything!
                     </p>
+                    <NormalButton
+                        onClick={() => {
+                            router.push("/editor");
+                        }}
+                        className="w-[200px] mt-4 mx-auto"
+                    >
+                        Try it out
+                    </NormalButton>
                     <div
                         className="border-[2px] border-gray-500 hover:border-gray-600 rounded-[100px] w-[40px] h-[40px] absolute bottom-8 left-[calc(50%-20px)] cursor-pointer"
                         onClick={() =>
@@ -68,49 +78,88 @@ const HomePage: React.FC = () => {
                 </div>
                 <div className="text-3xl mt-32 font-mackinac">How it works</div>
                 <ThinLine />
-                <div className="flex flex-row justify-center space-x-8 mt-10">
-                    <div className="text-lg">
-                        1. Select a Checker for your type of writing
-                    </div>
-                    <div className="relative">
-                        <StoreFront
-                            storefront={{
-                                objInfo: {
-                                    name: "Rizzume",
-                                    desc: "Rizz up your resume to dazzle any employer. It will make points sharp and salient. All to make you sound impressive.",
-                                    id: createShortId(),
-                                    creatorId: "fakeuser",
-                                },
-                                placeholder: "paste yourresume",
-                            }}
-                            isDemo={true}
+                <div className=" text-left max-w-[70%] justify-center flex flex-col mx-auto">
+                    <div className="flex flex-row justify-center space-x-8 mt-10 relative">
+                        <div className="text-lg flex-1">
+                            1. Select a Checker for your type of writing
+                        </div>
+                        <div className="flex-1">
+                            <StoreFront
+                                storefront={{
+                                    objInfo: {
+                                        name: "Rizzume",
+                                        desc: "Rizz up your resume to dazzle any employer. It will make points sharp and salient. All to make you sound impressive.",
+                                        id: createShortId(),
+                                        creatorId: "fakeuser",
+                                    },
+                                    placeholder: "paste yourresume",
+                                }}
+                                isDemo={true}
+                            />
+                            <CursorIcon className="w-[40px] h-[40px] right-32 bottom-[-20px] absolute" />
+                        </div>
+                        <Image
+                            alt="MushyChecklet"
+                            src={MushyChecklet.src}
+                            width={200}
+                            height={200}
+                            className="h-[5rem] top-[5rem] left-[10%] absolute"
                         />
-                        <CursorIcon className="w-[40px] h-[40px] right-4  bottom-[-20px] absolute" />
+                    </div>
+
+                    <div className="flex flex-row justify-center space-x-8 mt-32 relative">
+                        <div className="text-lg flex-1">
+                            2. Paste your writing into the editor
+                        </div>
+                        <div className="relative  w-[300px] flex-1">
+                            <div className="text-3xl font-mackinac text-gray-400">
+                                Rizzume
+                            </div>
+                            <div className="text-md text-gray-400">
+                                Rizz up your resume to dazzle...
+                            </div>
+                            <hr className="bg-black w-full h-[2px]" />
+                            <div>Grammarly • January 2021 - Present</div>
+                            <div>
+                                • Expedited DynamoDB queries from 68 ms to 41 ms
+                                by optimizing the schema for reads
+                            </div>
+                            <div>...</div>
+                        </div>
                         <Image
                             alt="PennyChecklet"
                             src={PennyChecklet.src}
                             width={200}
                             height={200}
-                            className="h-[7rem] top-[10rem] right-[20%] absolute"
+                            className="h-[7rem] top-[1rem] right-[-50%] md:right-[-30%] absolute"
                         />
                     </div>
-                </div>
+                    <div className="flex flex-row justify-center space-x-8 mt-8 ">
+                        <div className="text-lg flex-1">
+                            3. Receive instant feedback
+                        </div>
 
-                <div className="flex flex-row justify-center space-x-8 mt-40">
-                    <div className="text-lg">
-                        2. Paste your writing into the editor
+                        <div className="flex-1">
+                            <div className="max-w-[350px]">
+                                <CheckPreview
+                                    blueprint={{
+                                        objInfo: {
+                                            id: "",
+                                            name: "",
+                                            desc: "",
+                                            creatorId: "",
+                                        },
+                                        instruction: "",
+                                        category: "",
+                                        checkType: CheckType.rephrase,
+                                        positiveExamples: [],
+                                    }}
+                                    originalText=""
+                                    editedText=""
+                                />
+                            </div>
+                        </div>
                     </div>
-                    <div className="relative"></div>
-                </div>
-                <Image
-                    alt="MushyChecklet"
-                    src={MushyChecklet.src}
-                    width={200}
-                    height={200}
-                    className="h-[5rem] top-[70rem] right-[30%] absolute"
-                />
-                <div className="flex flex-row">
-                    <div className="text-2xl">3. Receive instant feedback</div>
                 </div>
                 <NormalButton
                     onClick={() => {
