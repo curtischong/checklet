6751bb3074d74804f23a01bb34aae72a765d847b
Curtis Chong <curtischong5@gmail.com>
2023-12-15 14:37:37 -0500

    added bricolage in header

--- checklet/src/components/Button.tsx
+++ checklet/src/components/Button.tsx
@@ -214,7 +214,7 @@ export const LinkButton: React.FC<
             // target="_blank"
             aria-label="Read research paper"
             className={classNames(
-                "pt-1 pb-2 pl-5 pr-7 inline-block rounded-md hover:text-white hover:bg-[#639fff] hover:border-[#639fff] relative text-primary border-[1px] border-gray-400 leading-[130%] tracking-normal transition duration-300",
+                "py-1 pl-5 pr-7 inline-block rounded-md hover:text-white hover:bg-[#639fff] hover:border-[#639fff] relative text-primary border-[1px] border-gray-400 leading-[130%] tracking-normal transition duration-300",
                 className,
             )}
             style={{
