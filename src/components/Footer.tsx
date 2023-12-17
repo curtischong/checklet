772a3bc63c9f113151a68f34b9107b49adab75d8
Curtis Chong <curtischong5@gmail.com>
2023-12-17 14:03:22 -0500

    updated footer with contact info + github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { GithubIcon } from "@/components/icons/GithubIcon";
 import classNames from "classnames";
 import Link from "next/link";
 
@@ -9,7 +10,7 @@ export const Footer = ({ isAbsolute }: Props) => {
     return (
         <div
             className={classNames(
-                "bg-[#f7b7b8] w-full py-4 mt-10 flex flex-row space-x-4 justify-center bottom-0",
+                "bg-[#f7b7b8] w-full py-4 mt-10 flex flex-row gap-x-8 gap-y-3 justify-center bottom-0 flex-wrap",
                 {
                     absolute: isAbsolute,
                 },
@@ -17,6 +18,10 @@ export const Footer = ({ isAbsolute }: Props) => {
         >
             <Link href="/privacy-policy">Privacy Policy</Link>
             <Link href="/terms-of-service">Terms of Service</Link>
+            <div>curtischong5@gmail.com</div>
+            <Link href="https://github.com/curtischong/checklet">
+                <GithubIcon className="cursor-pointer" />
+            </Link>
         </div>
     );
 };
