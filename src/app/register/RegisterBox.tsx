245f20e38e11a93c80391caef9faddf63290ecdb
Curtis Chong <curtischong5@gmail.com>
2024-09-18 12:59:58 -0400

    register box looks good

--- src/app/register/RegisterBox.tsx
+++ src/app/register/RegisterBox.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { useState } from "react";
+import type { FormEvent } from "react";
 import Link from "next/link";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { app } from "@/server/firebase/firebase";
 
-export default function Register() {
+export const RegisterBox = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmation, setConfirmation] = useState("");
@@ -33,11 +34,8 @@ export default function Register() {
 
   return (
     <main className="flex flex-col items-center justify-center p-8">
-      <div className="w-full rounded-lg shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
+      <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border">
         <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Checklet
-          </h1>
           <form
             onSubmit={handleSubmit}
             className="space-y-4 md:space-y-6"
@@ -107,7 +105,7 @@ export default function Register() {
             )}
             <button
               type="submit"
-              className="focus:ring-primary-300 dark:focus:ring-primary-800 w-full rounded-lg bg-gray-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 dark:bg-gray-600 dark:hover:bg-gray-700"
+              className="focus:ring-primary-300 dark:focus:ring-primary-800 hover:bg-primary2 w-full rounded-lg bg-primary px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
             >
               Create an account
             </button>
@@ -115,7 +113,7 @@ export default function Register() {
               Already have an account?{" "}
               <Link
                 href="/signin"
-                className="font-medium text-gray-600 hover:underline dark:text-gray-500"
+                className="font-medium text-primary hover:underline"
               >
                 Sign in here
               </Link>
@@ -125,4 +123,4 @@ export default function Register() {
       </div>
     </main>
   );
-}
+};
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,16 @@
-import Register from "@/app/register/Register";
+import { RegisterBox } from "@/app/register/RegisterBox";
+import { SignInPageBackground } from "@/app/signin/SignInPageBackground";
 
-export default function Page() {
-  return <Register />;
+export default async function Page() {
+  // TODO: what to do if the user is already signed in?
+  // this should be handled in the middleware. redirect to /checkers
+
+  return (
+    <SignInPageBackground>
+      <div className="mb-4 text-center">
+        Want to create a checker? Sign in/sign up below!
+      </div>
+      <RegisterBox />
+    </SignInPageBackground>
+  );
 }
