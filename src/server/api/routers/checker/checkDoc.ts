128a7716ecaa2aa06abad2029e7d530585f3037d
Curtis Chong <curtischong5@gmail.com>
2024-09-21 10:46:11 -0400

    wip starting to write prompts to extract resume tips

--- src/server/api/routers/checker/checkDoc.ts
+++ src/server/api/routers/checker/checkDoc.ts
@@ -1,7 +1,22 @@
 import { type CheckerType } from "@/server/api/routers/checker/checker";
+import { Llm } from "@/server/api/routers/checker/llm";
+import { SimpleCache } from "@/server/api/routers/checker/simpleCache";
+import path from "path";
 
 export const checkDoc = async (doc: string, checker: Awaited<CheckerType>) => {
-  console.log("checkDoc", checker, doc);
+  const systemPrompt = "";
+  const modelName = "gpt-3.5-turbo";
+  const apiKey = process.env.OPENAI_API_KEY;
+
+  const cache = new SimpleCache(
+    path.join(process.cwd(), ".chatgpt_history"),
+    "/cache",
+  );
+  const llm = new Llm(systemPrompt, modelName, cache, apiKey);
+  const res = await llm.prompt("hello");
+  console.log("res", res);
+
+  // console.log("checkDoc", checker, doc);
 
   return {
     suggestions: [],
---
+++ src/server/api/routers/checker/prompts.ts
@@ -0,0 +1,7 @@
+export const preprocessInstructions = `Given the below instructions, extract all of the tips/tricks/heuristics into a numbered list.
+
+Surround each tip with <tip></tip> tags. If the instruction also has a reason for the tip, include that as well right after the tip tag
+with a <reason></reason> tag.
+---INSTRUCTIONS---
+
+`;
