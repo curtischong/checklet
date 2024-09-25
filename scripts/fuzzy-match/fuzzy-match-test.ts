6bd1380366f6f79a198724c915bb74092d18e489
Curtis Chong <curtischong5@gmail.com>
2024-09-25 10:13:16 -0400

    more tests

--- scripts/fuzzy-match/fuzzy-match-test.ts
+++ scripts/fuzzy-match/fuzzy-match-test.ts
@@ -9,7 +9,9 @@ const assertEqual = (a: any, b: any) => {
 };
 
 {
-  console.log("test real text has more text (contains the 'C' lanaguage)");
+  console.log(
+    "----------------test real text has more text (contains the 'C' lanaguage)",
+  );
   const doc1 = "Backend Languages: C++, C, Golang, Python, Node.js, TypeScript";
   const _doc2 =
     "Backend Languages: C++, Golang, <tip|remove languages|we should rm these languages><old>Python,</old><new></new></tip>Node.js, TypeScript";
@@ -26,7 +28,7 @@ const assertEqual = (a: any, b: any) => {
 }
 
 {
-  console.log("test match with punctuation in text");
+  console.log("----------------test match with punctuation in text");
   const doc1 = "Backend Languages: C++, C, Golang, Python, Node.js, TypeScript";
   const _doc2 =
     "Backend Languages: C++, C, Golang<tip|remove languages|we should rm these languages><old>, Python, Node.js</old><new></new></tip>, TypeScript";
@@ -41,3 +43,59 @@ const assertEqual = (a: any, b: any) => {
   assertEqual(matchedSubstring, ", Python, Node.js");
   assertEqual(actualIndex, 33);
 }
+{
+  console.log("----------------test match with punctuation in text2");
+  const doc1 =
+    "Software Engineering Intern at Kik Interactive (Python, Docker, SQL) May - Aug. 2019";
+  const _doc2 =
+    "Software Engineering Intern at Kik Interactive (Python, Docker, SQL) May - Aug <tip|Consistent Abbreviation|It improves consistency in presentation><old>. 2019</old><new> 2019</new></tip>";
+
+  const expectedIndex = 79;
+  const { matchedSubstring, actualIndex } = fuzzyMatchAroundIndex(
+    doc1,
+    ". 2019",
+    expectedIndex,
+  );
+
+  assertEqual(matchedSubstring, ". 2019");
+  assertEqual(actualIndex, 78);
+}
+
+{
+  console.log(
+    "----------------test the spacing we're matching is not exactly the same as in the original doc",
+  );
+  const doc1 = "graduation date: 2019 (expected)";
+  const _doc2 =
+    "graduation date: 2019 <tip|fix expected|we shold fix expected><old> (expected)</old><new>(exp.)</new></tip> ";
+
+  const expectedIndex = 22;
+  const { matchedSubstring, actualIndex } = fuzzyMatchAroundIndex(
+    doc1,
+    " (expected)",
+    expectedIndex,
+  );
+
+  assertEqual(matchedSubstring, "(expected)");
+  assertEqual(actualIndex, 22);
+}
+
+// {
+//   console.log(
+//     "----------------test the text we're matching had an extra word that is not in the original doc",
+//   );
+//   const doc1 =
+//     "This is my graduation date: Honours Bachelor of Software Engineering Co-op | University of Waterloo | 2018 - 2023 (Expected)";
+//   const _doc2 =
+//     "This is my graduation date: <tip|fix expected|we shold fix expected><old>Honours Bachelor of Software Engineering Co-op | University of Waterloo | 2018 - 2023 (Expected)</old><new>Software Engineering Co-op | University of Waterloo | 2018 - 2023</new></tip>";
+
+//   const expectedIndex = 22;
+//   const { matchedSubstring, actualIndex } = fuzzyMatchAroundIndex(
+//     doc1,
+//     "Bachelor of Software Engineering Co-op | University of Waterloo | 2018 - 2023 (Expected)",
+//     expectedIndex,
+//   );
+
+//   assertEqual(matchedSubstring, "(expected)");
+//   assertEqual(actualIndex, 22);
+// }
