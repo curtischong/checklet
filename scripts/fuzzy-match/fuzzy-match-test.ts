023047f59ee660b5ef885817f3642276fdcc356c
Curtis Chong <curtischong5@gmail.com>
2024-09-25 10:02:16 -0400

    more tests

--- scripts/fuzzy-match/fuzzy-match-test.ts
+++ scripts/fuzzy-match/fuzzy-match-test.ts
@@ -1,13 +1,33 @@
-import { fuzzyMatchAroundIndex } from "@/server/api/routers/checker/fuzzyMatch5";
-import { assert } from "console";
+import { fuzzyMatchAroundIndex } from "@/server/api/routers/checker/fuzzyMatch3";
 
 const assertEqual = (a: any, b: any) => {
-  assert(a === b, `${a} !== ${b}`);
+  if (a !== b) {
+    console.log(`assertion failed, ${a} !== ${b}`);
+  } else {
+    console.log(`assertion passed, ${a}`);
+  }
 };
 
 {
+  console.log("test real text has more text (contains the 'C' lanaguage)");
   const doc1 = "Backend Languages: C++, C, Golang, Python, Node.js, TypeScript";
-  // const doc2 = "Python, Node.js,";
+  const _doc2 =
+    "Backend Languages: C++, Golang, <tip|remove languages|we should rm these languages><old>Python,</old><new></new></tip>Node.js, TypeScript";
+
+  const expectedIndex = 32;
+  const { matchedSubstring, actualIndex } = fuzzyMatchAroundIndex(
+    doc1,
+    ", Python, Node.js",
+    expectedIndex,
+  );
+
+  assertEqual(matchedSubstring, "Python,");
+  assertEqual(actualIndex, 35);
+}
+
+{
+  console.log("test match with punctuation in text");
+  const doc1 = "Backend Languages: C++, C, Golang, Python, Node.js, TypeScript";
   const _doc2 =
     "Backend Languages: C++, C, Golang<tip|remove languages|we should rm these languages><old>, Python, Node.js</old><new></new></tip>, TypeScript";
 
