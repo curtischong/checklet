ba7fd41ecc5a882a87b0ced5739dd44664e4613e
Curtis Chong <curtischong5@gmail.com>
2024-10-01 10:54:16 -0700

    cleanup

--- src/app/checker/[checkerId]/EditorPage.tsx
+++ src/app/checker/[checkerId]/EditorPage.tsx
@@ -3,7 +3,6 @@ import { Editor } from "@/app/checker/[checkerId]/editor/Editor";
 import { type GetCheckerByIdType } from "@/server/api/routers/checker/checker";
 import React from "react";
 
-// export const EditorTopPadding = 50;
 interface Props {
   checker: NonNullable<GetCheckerByIdType>;
 }
@@ -12,7 +11,8 @@ export const EditorPage = ({ checker }: Props) => {
   const [editorState, setEditorState] = React.useState("");
 
   return (
-    // <div className={`mx-10 h-full mt-[${EditorTopPadding}px]`}>
+    // try not to yolo add margin or padding here. since it will mess up the position of the suggestions box. add margine individually to the suggestion box and the textbox
+    // look for marginTop: "30px", in Editor.tsx (this adds margin individually to the textbox)
     <div className={`mx-10 h-full`}>
       <Editor
         checkerStorefront={{
