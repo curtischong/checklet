a4acd31b5cd856e45cc898323c1400f33ad8aeb9
Curtis Chong <curtischong5@gmail.com>
2024-10-03 16:24:41 -0700

    redirect to dashboard page via middleware

--- src/app/checkers/CreateOwnChecker.tsx
+++ src/app/checkers/CreateOwnChecker.tsx
@@ -1,26 +1,15 @@
-"use client";
-import { useClientCtx } from "@/app/ClientCtx";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export const CreateOwnChecker = () => {
-  const router = useRouter();
-  const { user } = useClientCtx(); // this user object cannnot be passed in from a server-side rendered prop since it can be outdated
-
   return (
     <div className="mx-auto mt-2">
       Can&lsquo;t find a checker you like?{" "}
-      <span
+      <Link
+        href="/dashboard?redirect-reason=create-checker"
         className="mx-auto mt-4 cursor-pointer border-b-2 border-blue-500 hover:text-blue-600"
-        onClick={() => {
-          if (!user) {
-            router.push("/signin?redirect-reason=create-checker");
-          } else {
-            router.push(`/dashboard`);
-          }
-        }}
       >
         Create your own Checker
-      </span>
+      </Link>
     </div>
   );
 };
