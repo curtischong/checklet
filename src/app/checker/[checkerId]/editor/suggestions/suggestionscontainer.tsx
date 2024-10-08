d8bcd40bde38314dfd8558a0430eeae511d9058e
Curtis Chong <curtischong5@gmail.com>
2024-10-08 01:42:22 -0400

    add padding at the bottom of suggestion list

--- src/app/checker/[checkerId]/editor/suggestions/suggestionscontainer.tsx
+++ src/app/checker/[checkerId]/editor/suggestions/suggestionscontainer.tsx
@@ -239,7 +239,7 @@ export const SuggestionsContainer: React.FC<Props> = ({
         />
       </div>
       <div
-        className="px-6"
+        className="px-6 pb-10"
         style={{
           // add up all the heights and margin tops of the elements above
           maxHeight: "calc(100vh - 50px - 40px - 5px - 5px - 40px - 5px)",
