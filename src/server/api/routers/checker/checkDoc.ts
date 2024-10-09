cd1a205ab75a28034508361fe59b974ce52f16f1
Curtis Chong <curtischong5@gmail.com>
2024-10-09 16:24:49 -0400

    update the prompt so the instructions are called [Instruction 1], so there's a rarer chance it clashes with the 1) ppl may use in prompts

--- src/server/api/routers/checker/checkDoc.ts
+++ src/server/api/routers/checker/checkDoc.ts
@@ -27,6 +27,7 @@ import {
   addTipTags4Dot8,
   inference6,
   inference6Dot2,
+  inference6Dot3,
   inferenceInstructions,
   inferenceInstructions1,
   inferenceInstructions1dot11,
@@ -608,6 +609,35 @@ export const checkDoc4Dot8 = async (
   const chain = await llm.promptMessagesExtendChain(
     [],
     inference6(prompt, doc),
+    llm.model,
+  );
+  console.log(
+    "doc2PlusChainOfThought---------------------------",
+    chain[chain.length - 1]!.content,
+  );
+  const rawDoc3 = await llm.promptMessagesExtendChain(
+    chain,
+    addTipTags4Dot8(),
+    llm.model,
+  );
+  const doc3 = removeInvalidTips(
+    rawDoc3[rawDoc3.length - 1]!.content as string,
+  ); // removes extraneous whitespace / removals the llm made
+  console.log("doc3---------------------------------", doc3);
+
+  const suggestions = extractSuggestions(doc, doc3);
+
+  return removeInvalidSuggestions(suggestions);
+};
+
+export const checkDoc4Dot9 = async (
+  llm: Llm3,
+  prompt: string,
+  doc: string,
+): Promise<Suggestion[]> => {
+  const chain = await llm.promptMessagesExtendChain(
+    [],
+    inference6Dot3(prompt, doc),
     llm.model,
   );
   console.log(
