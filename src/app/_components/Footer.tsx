79be80f15bb6f3393601fd3de6f65cd6e318c4bf
Curtis Chong <curtischong5@gmail.com>
2024-10-09 15:18:51 -0400

    add link to discord

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,4 +1,6 @@
+import Discord from "@public/logos/discord.svg";
 import classNames from "classnames";
+import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
@@ -9,7 +11,7 @@ export const Footer = ({ isAbsolute }: Props) => {
   return (
     <div
       className={classNames(
-        "bottom-0 mt-[30px] flex h-[55px] w-full flex-row flex-wrap justify-center gap-x-8 gap-y-3 bg-[#f7b7b8] py-4",
+        "bottom-0 mt-[30px] flex min-h-[55px] w-full flex-row flex-wrap justify-center gap-x-8 gap-y-3 bg-[#f7b7b8] py-4",
         {
           absolute: isAbsolute,
         },
@@ -19,12 +21,15 @@ export const Footer = ({ isAbsolute }: Props) => {
       <Link href="/terms-of-service">Terms of Service</Link>
       <Link href="/about">About</Link>
       <div>curtischong5@gmail.com</div>
-      {/* <Link
-                target="_blank"
-                href="https://github.com/curtischong/checklet"
-            >
-                <GithubIcon className="cursor-pointer" />
-            </Link> */}
+      <Link target="_blank" href="https://discord.gg/Gx9jXq3BaC">
+        <Image
+          src={Discord as string}
+          alt={`Discord logo`}
+          className="mr-4 h-6 w-6"
+          width={40}
+          height={40}
+        />
+      </Link>
     </div>
   );
 };
