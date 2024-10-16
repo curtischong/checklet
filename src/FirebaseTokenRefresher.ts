a36baae9e1b1ebcb380d3f9fd1a2b35346e56eeb
Curtis Chong <curtischong5@gmail.com>
2024-10-15 22:42:58 -0400

    add comment explaining more places where things can fail

--- src/FirebaseTokenRefresher.ts
+++ src/FirebaseTokenRefresher.ts
@@ -1,4 +1,6 @@
 // https://chatgpt.com/share/670f2792-7db4-800e-8730-c2d32211fc51
+
+// it seems like you need to manually refresh the token in firebase auth edge: https://github.com/awinogrodzki/next-firebase-auth-edge/issues/14
 import { getAuth } from "firebase/auth";
 import { useEffect } from "react";
 
@@ -20,6 +22,9 @@ export const useFirebaseTokenRefresher = (refreshInterval = 30 * 60 * 1000) => {
         });
       }
     };
+
+    // TODO: do we need to refresh the token initially? I think we do? I'll enable it if ppl complain
+    // refreshToken();
 
     // Set up periodic refresh
     const interval = setInterval(() => {
