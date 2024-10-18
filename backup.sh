8d0742381a4c84ba477c6782efca27b93f1f269b
Curtis Chong <curtischong5@gmail.com>
2024-10-18 14:39:03 -0400

    backup script now takes a param for the password

--- backup.sh
+++ backup.sh
@@ -5,8 +5,8 @@ DB_NAME="checklet"
 DB_USER="postgres"
 # BACKUP_DIR="/home/ubuntu/db_backups"
 BACKUP_DIR="/Users/curtischong/Documents/dev/checklet/backups"
-DB_PASSWORD="<fill before you backup>"
-DATE=$(date +\%Y-\%m-\%d)
+DB_PASSWORD=$1
+TIMESTAMP=$(date +%s)
 
 # Docker container details
 DB_HOST="100.27.18.241" # Replace this with your container's IP or hostname
@@ -15,8 +15,8 @@ DB_PORT="5432" # Default PostgreSQL port
 # Export password to environment variable
 export PGPASSWORD=$DB_PASSWORD
 
-# Create backup
-pg_dump -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_NAME > $BACKUP_DIR/db_backup_$DATE.sql
+# Create backup with Unix timestamp as the filename
+pg_dump -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_NAME > $BACKUP_DIR/db_backup_$TIMESTAMP.sql
 
 # Optional: Remove old backups (older than 7 days in this case)
 find $BACKUP_DIR/* -mtime +7 -exec rm {} \;
