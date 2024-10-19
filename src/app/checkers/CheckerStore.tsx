8c3f62c00df3b2295bc96352d397ba7c10099b31
Curtis Chong <curtischong5@gmail.com>
2024-10-19 12:06:00 -0400

    show user vs public checkers (#151)

--- src/app/checkers/CheckerStore.tsx
+++ src/app/checkers/CheckerStore.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { CheckerDesc } from "@/app/checker/[checkerId]/editor/CheckerDesc";
+import { useClientCtx } from "@/app/ClientCtx";
 import { useTrpcCtx } from "@/app/TrpcCtx";
 import { handleErr } from "@/trpc/react";
 import { type Checker } from "@prisma/client";
@@ -9,15 +10,64 @@ import { useEffect, useState } from "react";
 
 export const CheckerStore = (): JSX.Element => {
   const [checkers, setCheckers] = useState<Checker[]>([]);
+  const [userCheckers, setUserCheckers] = useState<Checker[]>([]);
   const { trpcClient } = useTrpcCtx();
+  const { user } = useClientCtx();
 
   useEffect(() => {
-    handleErr(trpcClient.checker.getAllCheckers.query(), setCheckers);
+    handleErr(trpcClient.checker.getAllCheckers.query(), (checkers) => {
+      const userCheckers = [];
+      const nonUserCheckers = [];
+      for (const checker of checkers) {
+        if (checker.createdById === user?.id) {
+          userCheckers.push(checker);
+        } else {
+          nonUserCheckers.push(checker);
+        }
+      }
+      setCheckers(nonUserCheckers);
+      setUserCheckers(userCheckers);
+    });
   }, []);
+
+  if (!user) {
+    return (
+      <div className="mt-10 flex flex-col items-center space-y-6">
+        {checkers.map((checker) => {
+          return (
+            <div key={checker.id}>
+              <StoreFront checker={checker} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 
   return (
     <div className="mt-10 flex flex-col items-center space-y-6">
       {/* TODO: add a search bar */}
+      {user && (
+        <h2 className="text-center font-mackinac text-2xl font-bold">
+          Your Checkers
+        </h2>
+      )}
+      {userCheckers ? (
+        <div className="flex flex-col space-y-4">
+          {userCheckers.map((checker) => {
+            return (
+              <div key={checker.id}>
+                <StoreFront checker={checker} />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <h2>You have no checkers</h2>
+      )}
+      <h2 className="text-center font-mackinac text-2xl font-bold">
+        Public Checkers
+      </h2>
       {checkers.map((checker) => {
         return (
           <div key={checker.id}>
