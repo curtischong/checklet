4b2c0b30f77085db9ebca5416a585debfe05143e
Curtis Chong <curtischong5@gmail.com>
2024-10-21 11:39:51 -0400

    prompts to extract writing style

---
+++ scripts/extract-writing-style/writing-style-prompts.ts
@@ -0,0 +1,16 @@
+export const initialDefine = `
+identify the author's writing style.
+
+- how much detail they provide
+- How long are the author's sentences?
+- What word order do they use? The basic structure in English is SVO, but how does the author change this up?
+- Where do they place interjection phrases, etc.?
+- How many adjectives/adverbs does the author use? Per sentence? Per paragraph?
+- How many conjunctions? *Which* conjunctions? Are they comfortable starting sentences with conjunctions?
+- What is their punctuation use like? Consider commas, (semi)colons, parenthesis, dashes, etc.
+- Does the author use a comma after every phrase, or do they prefer run-on sentences?
+- Do they use Oxford comma or no? How frequently do they use (semi)colons? For what purpose?
+- Do they use parenthesis as character asides, as opposed to authorial inserts, etc.?
+- What dash style do they use? (En dashes or em dashes? Do they frame the dashes with spaces? In what situation?)`;
+
+export const rephraseLearningsAsBulletPoints = `rephrase what you've learned as a set of bullet points that describe how one might write in the same format as the text.`;
